5f2ce9b70508525f6bb09ba29a97656689ee0313
GJCreatorSmpSeizoen1 <jaydenderooi0@gmail.com>
2026-09-19 23:26:27 +0200

    Create spelers.js

---
+++ spelers.js
@@ -0,0 +1,42 @@
+const spelers = [
+{
+id: "jorn",
+naam: "Jorn",
+rol: "Creator",
+status: "levend",
+kills: 0,
+deaths: 0,
+events: 0,
+skin: "https://mc-heads.net/avatar/Jorn/300",
+beschrijving: "Jorn is een deelnemer van GJ Creator SMP Seizoen 1."
+},
+
+{
+    id: "sem",
+    naam: "Sem",
+    rol: "Creator",
+    status: "levend",
+    kills: 0,
+    deaths: 0,
+    events: 0,
+    skin: "https://mc-heads.net/avatar/Sem/300",
+    beschrijving: "Sem is een deelnemer van GJ Creator SMP Seizoen 1."
+}
+
+/*
+NIEUWE SPELER:
+
+{
+    id: "naam",
+    naam: "Naam",
+    rol: "Creator",
+    status: "levend",
+    kills: 0,
+    deaths: 0,
+    events: 0,
+    skin: "https://mc-heads.net/avatar/Naam/300",
+    beschrijving: "Naam is een deelnemer van GJ Creator SMP Seizoen 1."
+}
+*/
+
+];
