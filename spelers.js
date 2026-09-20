4fa66f73d51cef035dfc590291b155e5c42859f2
GJCreatorSmpSeizoen1 <jaydenderooi0@gmail.com>
2026-09-20 21:59:12 +0200

    Update spelers.js

--- spelers.js
+++ spelers.js
@@ -4,22 +4,45 @@ const spelers = [
         id: "jorn",
         naam: "Jorn",
         rol: "Creator",
+
         status: "levend",
+
+        zichtbaar: true,
+
+        // Verschijnt op 20 september 2026 om 22:05
+        zichtbaarVanaf: "2026-09-20T22:05:00+02:00",
+
         kills: 0,
+
         events: 0,
-        skin: "https://mc-heads.net/avatar/Jorn/300",
+
+        skin:
+            "https://mc-heads.net/avatar/Jorn/300",
+
         beschrijving:
             "Jorn is een deelnemer van GJ Creator SMP Seizoen 1."
     },
+
 
     {
         id: "sem",
         naam: "Sem",
         rol: "Creator",
+
         status: "levend",
+
+        zichtbaar: true,
+
+        // Verschijnt op 20 september 2026 om 22:05
+        zichtbaarVanaf: "2026-09-20T22:05:00+02:00",
+
         kills: 0,
+
         events: 0,
-        skin: "https://mc-heads.net/avatar/Sem/300",
+
+        skin:
+            "https://mc-heads.net/avatar/Sem/300",
+
         beschrijving:
             "Sem is een deelnemer van GJ Creator SMP Seizoen 1."
     }
